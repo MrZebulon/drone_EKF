3decb869f3bf125d03ef897f997f0b25d4be03cd
BuildTools <unconfigured@null.spigotmc.org>
2023-03-17 14:03:34 +0100

    typo

--- data_reader.m
+++ data_reader.m
@@ -5,6 +5,6 @@ function trajData = import_data(file)
     
     T = readtable(file);
     as_quat = quaternion(T(:, [7, 8, 9, 10]));
-    T = table([T{:, 1:6}, as_quat], "VariableNames", headers);
+    T = table([T{:, 1:6} as_quat], "VariableNames", headers);
     trajData = table2struct(T, "ToScalar",true);
 end
